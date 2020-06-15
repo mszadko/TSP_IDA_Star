0af34a9618597584076cd4ec311c969ac751ff94
mszadko <ma.szadkowski@gmail.com>
2020-06-15 18:09:21 +0200

    Added first heuristic, it's calculated properly but I'm not using it so far

--- IDA_Star_TSP/IDASTAR/main.cpp
+++ IDA_Star_TSP/IDASTAR/main.cpp
@@ -6,7 +6,7 @@
 #include <stack>
 
 using namespace std;
-
+vector<string> cities = { "Warszawa", "Lodz", "Katowice", "Krakow", "Lublin","Kielce" };
 const int WWA = 0;
 const int LDZ = 1;
 const int KAT = 2;
@@ -47,13 +47,78 @@ bool DoesElementExist(std::vector<SearchNode> vector, int data)
 	return false;
 }
 
+int h1(vector<SearchNode> visited, vector<int> unvisited, int actions[NumberOfCities][NumberOfCities], int startNode)
+{
+	if (unvisited.size() == 0)
+		return 0;
+	int h1 = 0;
+	int f = 0, t = 0;
+	int shortestConnection = INT_MAX;
+	//look for shortest way to connect visited cities with unvisited ones
+	for (size_t i = 0; i < visited.size(); i++)
+	{
+		int from = visited[i].cityID;
+		for (size_t j = 0; j < unvisited.size(); j++)
+		{
+			int to = unvisited[j];
+			if (from!=to&&actions[from][to]<INT_MAX)
+			{
+				if (actions[from][to]<shortestConnection)
+				{
+					shortestConnection = actions[from][to];
+					f = from;
+					t = to;
+				}
+			}
+		}
+	}
+	cout << "Connection from visited to unvisited = " << shortestConnection << " from " << cities[f]<< " to " << cities[t] << endl;
+	h1 = shortestConnection;
+	f = 0; t = 0;
+	//now lets find cheapest action that take us from every unvisited node to another unvisited node or to the start node
+	for (size_t i = 0; i < unvisited.size(); i++)
+	{
+		int shortestWayOut = INT_MAX;
+		int from = unvisited[i];
+		for (size_t j = 0; j < unvisited.size(); j++)
+		{
+			int to = unvisited[j];
+			if (from != to && actions[from][to] < INT_MAX)
+			{
+				if (actions[from][to] < shortestWayOut)
+				{
+					shortestWayOut = actions[from][to];
+					f = from;
+					t = to;
+				}
+				//we additionaly need to check way to start node
+				if (j == unvisited.size() - 1)
+				{
+					if (actions[from][startNode] < shortestWayOut)
+					{
+						shortestWayOut = actions[from][startNode];
+						f = from;
+						t = startNode;
+					}
+				}
+			}
+		}
+		if (f != t)
+		{
+			h1 += shortestWayOut;
+			cout << "Shortest way out from " << cities[f] << " to  " << cities[t] << "  costs " << shortestWayOut << endl;
+		}
+	}
+	return h1;
+}
+
 int main()
 {
 	stack<SearchNode> DepthFirstTravelsalStack;
-	vector<string> cities = { "Warszawa", "Lodz", "Katowice", "Krakow", "Lublin","Kielce" };
+	//  0-Warszawa , 1-Lodz, 2-Katowice , 3-Krakow, 4-Lublin, 5-Kielce 
 	int actions[NumberOfCities][NumberOfCities] =
 	{
-		{0, 135, 75, 300, 165, 180},
+		{0, 135, 300, 300, 165, 180},
 		{135, 0, 195, 270, 310, 140},
 		{300, 195, 0, 75, 305, 180},
 		{300, 270, 75, 0, 260, 120 },
@@ -61,9 +126,12 @@ int main()
 		{180, 140, 180, 120, 175, 0}
 	};
 
+
+
 	int start = WWA;
 	DepthFirstTravelsalStack.push(SearchNode(start, start));
 	vector<SearchNode> visited;
+	vector<int> unvisitedCities = { 0,1,2,3,4,5 };
 
 	while (!DepthFirstTravelsalStack.empty())
 	{
@@ -74,11 +142,22 @@ int main()
 		//check whether we make a return to some previous node in naive in depth search and make sure visited nodes are handled properly
 		while (visited.size() >= 1 && current.parentCityID != visited[visited.size() - 1].cityID)
 		{
+			int erased = visited[visited.size()-1].cityID;
 			visited.pop_back();
+			unvisitedCities.push_back(erased);
 		}
 		
 		//put new node on visited list
 		visited.push_back(current);
+		for (std::vector<int>::iterator i = unvisitedCities.begin(); i < unvisitedCities.end(); i++)
+		{
+			if (*i == current.cityID)
+			{
+
+				unvisitedCities.erase(i);
+				break;
+			}
+		}
 
 		//add childs of current onto stack so we can visit them later.
 		for (int i = NumberOfCities - 1; i >= 0; i--)
@@ -90,7 +169,7 @@ int main()
 		}
 
 		//this is just a debug.
-		if (visited.size() == NumberOfCities)
+		if (true)//visited.size() == NumberOfCities)
 		{
 			int length = 0;
 			for (size_t i = 0; i < visited.size(); i++)
@@ -99,9 +178,16 @@ int main()
 				length += actions[visited[i].parentCityID][visited[i].cityID];
 			}
 			cout <<"Warszawa\t\t Length = " << length + actions[visited[visited.size() - 1].cityID][start];
+			cout << "\nUNVISITED : ";
+			for (size_t i = 0; i < unvisitedCities.size(); i++)
+			{
+				cout << cities[unvisitedCities[i]] << ", ";
+			}
 			cout << endl;
 		}
 	
+		cout << "H1 = " << h1(visited, unvisitedCities, actions, start) << endl;
+
 		
 	}
 	return 0;
